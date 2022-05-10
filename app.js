ea4b9e151fd13849cd3ebfc3569080c4d91aa1f4
test <test@mail.com>
2022-05-10 02:30:39 -0700

    test GitOps v2

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello Gitops!'))
+app.get('/', (req, res) => res.send('Hello Gitops!!'))
 app.listen(3000, () => console.log('Server ready'))
