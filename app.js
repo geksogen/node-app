216179b65a7e8ad416f79dad65f912017a4a67c2
test <test@mail.com>
2022-05-16 05:19:47 -0700

    operations

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('GitOps maze faka!'))
+app.get('/', (req, res) => res.send('GitOps!'))
 app.listen(3000, () => console.log('Server ready'))
