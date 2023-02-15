6246d93e2e7c56b055012d0267cdfa5dc6f51e7e
Andrey.Torshin <dchorch@gmail.com>
2023-02-15 15:15:46 +0300

    v78

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('GitOps!!'))
+app.get('/', (req, res) => res.send('GitOps!'))
 app.listen(3000, () => console.log('Server ready'))
